87844ae8b101d2ae5fd83615e96fca5be7423415
Laura HL <40395141+lamusadecima@users.noreply.github.com>
2021-11-20 19:01:00 +0100

    Add files via upload

---
+++ Script1.R
@@ -0,0 +1,57 @@
+######################################################
+#                                                    #
+#                   INICIACIÓN A R                   #
+#                                                    #
+# Seminario del máster en Humanidades Digitales UNIR #
+#                                                    #
+#######################################################
+
+# Algunas funciones básicas de R
+
+# Funciones matemáticas en R
+
+1 + 1
+10-5
+10 * 4532
+45320 / 10
+10 * pi
+# Raíz cuadrada
+sqrt(144)
+#Para las potencias
+2^3
+#Valor absoluto
+abs(-4)
+#Redondear
+round(1.8)
+
+# Para entender los índices
+10:100
+
+# ¿Qué pasa si tecleas una orden incompleta como 3-?
+
+# Imprime
+print("HELLO WORLD!")
+
+# Para acceder a la documentación
+help()
+help(sqrt)
+
+#Crear variables
+x = 10
+10 -> x
+x <- 10
+
+# Inspeccionar una variable
+x
+
+# Algunas pruebas
+x-2
+x
+y = 5
+x + y
+x
+y
+z = x + y
+z
+k = "HELLO WORLD!"
+print(k)
